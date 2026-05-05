2005ca9946880827699e3f4bc63bdcf1144fa570
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-05-05 13:02:18 -0400

    Corrección de menú colapsable en componente layout

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
@@ -8,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Layout implements OnInit {
 
-  public isMenuCollapsed: boolean = true;
+  public isMenuCollapsed: boolean = false;
+  public barsIcon: IconDefinition = faBars;
 
   ngOnInit(): void {
 
