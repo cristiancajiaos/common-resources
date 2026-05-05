332d86cca6b7832f343072069f1707f237becbce
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-05-05 13:13:57 -0400

    Establecido contenido de vista Home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+
+  public modules: string[] = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
+  ngOnInit(): void {
+
+  }
+}
